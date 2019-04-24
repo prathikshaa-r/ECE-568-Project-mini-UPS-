7d0135890d2d779baca800b460f04e803fcdd4f8
David <david@vcm-8949.vm.duke.edu>
2019-04-24 07:38:40 -0400

    updated models

--- UPS_FrontEnd/UPS/models.py
+++ UPS_FrontEnd/UPS/models.py
@@ -8,18 +8,23 @@ class Warehouse(models.Model):
 
 class Truck(models.Model):
     truck_id = models.IntegerField(primary_key=True)
+    x = models.IntegerField(null = True, blank = True)
+    y = models.IntegerField(null = True, blank = True)
     status =  models.CharField(max_length=100,default="",null=True, blank=True)
 
 class Package(models.Model):
     packageid = models.IntegerField(primary_key=True)
     truck = models.ForeignKey(Truck, on_delete = models.CASCADE,null=True,blank=True,related_name = "package_set")
+    warehouse =  models.ForeignKey(Warehouse, on_delete = models.CASCADE,null=True,blank=True,related_name = "package_set") 
     
 class IncomingSeqWorld(models.Model):
     sequence_number = models.IntegerField(primary_key = True)
 
 class OutgoingSeqWorld(models.Model):
+    #Don't we need to save the message as well? To send it again?
     sequence_number = models.IntegerField(primary_key = True)
     acked = models.BooleanField(default = False)
+    message =  models.CharField(max_length=10000,default="",null=True, blank=True)
     
 class IncomingSeqUA(models.Model):
     sequence_number = models.IntegerField(primary_key = True)
@@ -27,3 +32,4 @@ class IncomingSeqUA(models.Model):
 class OutgoingSeqUA(models.Model):
     sequence_number = models.IntegerField(primary_key = True)
     acked = models.BooleanField(default = False)
+    message =  models.CharField(max_length=10000,default="",null=True, blank=True)
